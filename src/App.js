29a6b466ec60c946deb702f6ac00a95de03a01cf
TuanVT <vutrongtuan@gmail.com>
2024-12-31 17:17:06 +0700

    ff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import logo from "./logo.svg";
-import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import RouterCustom from "./router";
 import "./styles/style.scss";
--- src/page/users/layoutComponent/header/index.js
+++ src/page/users/layoutComponent/header/index.js
@@ -1,7 +1,62 @@
 import React, { memo } from "react";
-
+import "./styles.scss";
+import { AiOutlineFacebook } from "react-icons/ai";
+import { SiInstagram } from "react-icons/si";
+import { FaLinkedin, FaTwitter, FaUserCircle } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { formater } from "ultis/formater";
 function Header() {
-  return <div>fhskld</div>;
+  return (
+    <div className="header_top">
+      <div className="container">
+        <div className="row">
+          <div className="col-6 header_top_left">
+            <ul>
+              <li>
+                <MdEmail />
+                abudabi@gmail.com
+              </li>
+              <li>
+                Miễn phí ship từ đơn{" "}
+                <strong style={{ color: "#008874" }}>{formater(200000)}</strong>
+              </li>
+            </ul>
+          </div>
+          <div className="col-6 header_top_right">
+            <ul>
+              <li>
+                <Link to={""}>
+                  <AiOutlineFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <SiInstagram />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaTwitter />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaLinkedin />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaUserCircle />
+                </Link>
+                <span>Đăng nhập</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default memo(Header);
